2685879e0d77d45e9274a2e3cbc102c2e38e29e4
Nicholas Rosas <nicholasirosas@student.hartnell.edu>
2016-09-04 10:24:58 -0700

    Used python to receive dictionary from endpoint; then, found the needle in the haystack and sent results as dictionary to next endpoint

--- step_3.py
+++ step_3.py
@@ -1,27 +1,29 @@
-import requests
-import json
-import urllib
+import requests ## To be able to make requests to URL
+import json ## to work with json
+#import urllib ## In association with making requests, but not necessary
 
 def findNeedle(token):
-    toReceive = 'http://challenge.code2040.org/api/haystack'
-    toSend = 'http://challenge.code2040.org/api/haystack/validate'
+    toReceive = 'http://challenge.code2040.org/api/haystack' ## URL to receive dictionary
+    toSend = 'http://challenge.code2040.org/api/haystack/validate' ## URL to send result
     
     access_dict = dict() ## dictionary to access data
     result_dict = dict() ## dictionary to send result data
     
     access_dict['token'] = token
     
-    r = requests.post(toReceive, data=access_dict)
+    r = requests.post(toReceive, data=access_dict) ## Make request to endpoint for dictionary
     
     d = dict() ## temporary dictionary
-    d = r.json() ## to receive dictionary from requests
+    d = r.json() ## to place dictionary from requests
     
+    ##Finds the needle, then stores the index in the result dictionary along with the token
     needle = d['needle']
     result_dict['token'] = access_dict['token']
     result_dict['needle'] = d['haystack'].index(needle)
     
-    r = requests.post(toSend, data=result_dict)
+    r = requests.post(toSend, data=result_dict) ## Make request with our results
     
+    ## print out result message for (pass/fail)
     print r.text
     print r.status_code
     
